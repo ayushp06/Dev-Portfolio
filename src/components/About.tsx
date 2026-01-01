2ff4f39f922e40e17813a2c956bbd1cf73444be0
alter <apandejee006@yahoo.com>
2026-01-01 17:35:25 -0500

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,16 +22,7 @@ export function About() {
         
         <div className="max-w-3xl mx-auto mb-16">
           <p className="text-lg text-slate-700 mb-6">
-            Currently, I am a Data Engineer Intern for the School District of Philadelphia. I automate workflow by turning messy, manual data processes into streamlined systems. Using Java, I’ve helped teams reclaim 5-6 hours of their day by automating the heavy lifting, everything from cross-checking thousands of rows to building custom validation tools.
-          </p>
-          <p className="text-lg text-slate-700 mb-6">
-            I served as a Research Assistant in the Students Space Programs Laboratory (SSPL), where I developed a C++ flight computer integrated with barometric and gyroscopic sensors for a two-stage rocket that successfully launched to 3,000 ft. I also contributed to NASA's LunaRecycle Challenge, optimizing a metal-smelting system for lunar manufacturing using SOLIDWORKS and ANSYS Mechanical.
-          </p>
-          <p className="text-lg text-slate-700 mb-6">
-            As the Vehicle Dynamics Project Manager for Nittany Motorsports, I led lap-time simulation development in ChassisSIM and MATLAB to enhance vehicle setup and performance through data-driven engineering.
-          </p>
-          <p className="text-lg text-slate-700">
-            Outside the lab, I build full-stack and embedded systems from a Chrome productivity extension (LockedIn) using React, TypeScript, and Firebase to an AI Satellite Anomaly Detector and real-time 3D Orbit Tracker built with Python, TensorFlow, and PyQt5.
+            I attend Penn State as an Aerospace Engineering student with a minor in Information Sciences & Technology. I work part time as a Data Engineer Intern for the School District of Philadelphia. Additionally, I am a Brazilian Jiu-Jitsu athlete and a professional photographer. Scroll below to view my work!
           </p>
         </div>
       </div>
